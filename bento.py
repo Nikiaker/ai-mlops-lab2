215d35bee11d2648e0c62692b54ba364dcc3a628
nikiaker <md.macek@gmail.com>
2025-05-07 23:18:27 +0200

    fixed bento

--- bento.py
+++ bento.py
@@ -1,23 +1,24 @@
 import bentoml
-from bentoml.io import JSON
 import torch
 from train import LightningModel
 
-CHECKPOINT_PATH = "./checkpoints/best_model-epoch=03-val_loss=0.15.ckpt"
+CHECKPOINT_PATH = "./checkpoints/best_model-epoch=15-val_loss=0.09.ckpt"
 model = LightningModel.load_from_checkpoint(CHECKPOINT_PATH)
 model.eval()
 
-svc = bentoml.Service("model_service")
+@bentoml.service
+class InferenceService:
+    @bentoml.api
+    def predict(self, data: list) -> dict:
+        input_tensor = torch.tensor(data)
+        if torch.cuda.is_available():
+            input_tensor = input_tensor.cuda()
 
-@svc.api(input=JSON(), output=JSON())
-def predict(input_data):
-    input_tensor = torch.tensor(input_data["data"])
-    
-    with torch.no_grad():
-        predictions = model(input_tensor)
-        predicted_digit = predictions.argmax(dim=1).item()
-    
-    return {
-        "predicted_digit": predicted_digit,
-        "prediction_values": predictions.tolist(),
-    }
+        with torch.no_grad():
+            predictions = model(input_tensor)
+            predicted_digit = predictions.argmax(dim=1).item()
+        
+        return {
+            "predicted_digit": predicted_digit,
+            "prediction_values": predictions.tolist(),
+        }
